db2c0bf36d189a09e859dd22647fa5292a69b9b4
jhlee <wisdom_13_@naver.com>
2022-10-22 23:51:24 +0900

    마우스 따라가기

--- src/AppXY.jsx
+++ src/AppXY.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './AppXY.css';
 
 export default function AppXY() {
+
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
   return (
-    <div className='container'>
-      <div className='pointer' />
+    <div className='container'
+      onPointerMove={(e) => {
+        setPosition({ x: e.clientX, y: e.clientY });
+      }}
+    >
+      <div className='pointer' style={{
+        transform: `translate(${position.x}px, ${position.y}px)`
+      }} />
     </div>
   );
 }
