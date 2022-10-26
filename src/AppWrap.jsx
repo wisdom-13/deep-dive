58ae43cf8d455ab5650723369b7a78d958b5fba6
jhlee <wisdom_13_@naver.com>
2022-10-27 00:39:07 +0900

    컴포넌트의 재사용

---
+++ src/AppWrap.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+
+export default function AppForm() {
+
+  return (
+    <div>
+      <Navbar></Navbar>
+    </div>
+  );
+}
+
+function Navbar() {
+  return (
+    <header style={{ backgroundColor: 'yellow' }}>
+
+    </header>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import AppForm from './AppForm';
+import AppCard from './AppCard';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AppForm />
+    <AppCard />
   </React.StrictMode>
 );
 
